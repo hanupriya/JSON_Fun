be92a13a4859abac9f34c59ff8519cfefce95def
hanupriya <hanupriya28@gmail.com>
2017-02-03 23:44:56 +0530

    JSON Fun

--- JSON_Fun/ViewController.swift
+++ JSON_Fun/ViewController.swift
@@ -6,20 +6,52 @@
 //  Copyright © 2017 MadLab. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://api.fixer.io/latest")
+        //getting the data
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil
+            {
+                print ("ERROR") // if no data
+            }
+            else{ //putting the data into content
+                if let content = data
+                {
+                    do{ // converting the data into an array
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        //converting to dictionary
+                        if let rates = myJson["rates"] as? NSDictionary
+                        {// extracting an element from the dictionary using its object ID
+                            print ("The List of the value of the currencies are :",rates)
+                            if let currency = rates["INR"]
+                            {
+                                print (currency)
+                            }
+                        }
+                        
+                        
+                    }
+                    catch{
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
